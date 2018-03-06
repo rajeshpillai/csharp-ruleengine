7a4d0d3c2d831fd4e00f38ec62dab24743e06035
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-03-06 10:58:40 +0530

    Rule engine basic

---
+++ RulesEngine/Model/Registration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RulesEngine.Rules;
+
+namespace RulesEngine.Model
+{
+    [RuleEngineType(RuleType = typeof(DefaultRuleEngine<Registration>))]
+    public class Registration
+    {
+        [RequiredField("UserName", "User name cannot be empty")]
+        [MaxLenField("UserName", "Max 15 chars", 15)]
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        [RequiredField("Email", "Email cannot be empty")]
+        public string Email { get; set; }
+
+        [CompareField("Email", "Email is not the same")]
+        public string EmailConfirm { get; set; }
+    }
+}
---
+++ RulesEngine/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RulesEngine.Model;
+using RulesEngine.Rules;
+
+namespace RulesEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+    {
+            Registration register = new Registration();
+
+            IRuleEngine<Registration> ruleEngine = RuleEngineFactory<Registration>.GetEngine(); 
+
+            register.UserName = "rajeshrajeshrajeeshrajeshrajesh";
+            register.Password = "test123";
+            register.Email = "test@t.com";
+            register.EmailConfirm = "test@t.com";
+
+            var results = ruleEngine.Validate(register);
+
+            foreach (var r in results)
+            {
+                if (r.IsBroken)
+                {
+                    Console.WriteLine("{0} rule is broken and the error is {1}", r.Name, r.ErrorMessage);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
