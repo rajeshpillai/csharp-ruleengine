d3e0e18cc5c8312b0e62e0450f6f7c30d7bc5b82
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-03-06 11:59:02 +0530

    updated

--- RulesEngine/Program.cs
+++ RulesEngine/Program.cs
@@ -11,7 +11,7 @@ namespace RulesEngine
 
             IRuleEngine<Registration> ruleEngine = RuleEngineFactory<Registration>.GetEngine();
 
-            register.UserName = "rajeshrajeshrajeeshrajeshrajesh";
+            register.UserName = "rajesh";
             register.Password = "test123";
             register.Email = "";
             register.EmailConfirm = "test";
