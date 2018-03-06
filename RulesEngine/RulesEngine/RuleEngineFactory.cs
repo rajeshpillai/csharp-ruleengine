c701bcdbb4f7397d0e45ae4527eb9f0f29edff7d
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-03-06 12:03:25 +0530

    Refactor: Remove unused references

--- RulesEngine/RulesEngine/RuleEngineFactory.cs
+++ RulesEngine/RulesEngine/RuleEngineFactory.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RulesEngine.Rules;
-using System.Configuration;
+﻿using System.Configuration;
 using InterfaceDemo.RulesEngine;
-using RulesEngine.Model;
 
 namespace RulesEngine
 {
--- RulesEngine/RulesEngine/XmlRuleEngine.cs
+++ RulesEngine/RulesEngine/XmlRuleEngine.cs
@@ -2,9 +2,6 @@
 using RulesEngine.Rules;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace InterfaceDemo.RulesEngine
